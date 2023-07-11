4e4bfa86fc9f084018cea74397f2e40469d7dce1
jaythewebdev <jaythewebdev@gmail.com>
2023-07-12 03:19:02 +0530

    Exception Handling Initial

---
+++ ExceptionHandlingApp/InvalidAgeException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandlingApp
+{
+    internal class InvalidAgeException:Exception
+    {
+        string message;
+        public InvalidAgeException() {
+                message = "Not a valid age";
+            }
+            public InvalidAgeException(string message)
+            {
+                this.message = message;
+            }
+        public override string Message
+        {
+            get { return message; }
+        }
+    }
+    }
+}
---
+++ ExceptionHandlingApp/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Diagnostics;
+
+namespace ExceptionHandlingApp
+{
+    internal class Program
+    {
+        void SimpleMethod()
+        {
+            int num1 = 0, num2 = 0, result = 0;
+            try
+            {
+                try
+                {
+                    Console.WriteLine("Please enter the first number");
+                    num1 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Please enter the Second number");
+                    num2 = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (FormatException fe)
+                {
+                    Console.WriteLine("Don't even know how to type a number !!!");
+                    Debug.WriteLine(fe.Message);
+                }
+                result = num1 / num2;
+                Console.WriteLine("The result is " + result);
+            }
+            catch (ArithmeticException ae)
+            {
+                Console.WriteLine("Cannot divide by zero. Please start over and enter the second number > 0 ");
+                Debug.WriteLine(ae.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Oops !!! something went wrong");
+            }
+            finally
+            {
+                Console.WriteLine("Bye");
+            }
+        }
+
+        void Age()
+        {
+            int age = 0;
+            try
+            {
+                Console.WriteLine("Please ener teh age");
+                age = Convert.ToInt32(Console.ReadLine());
+                if (age < 18)
+                    throw new InvalidAgeException("the entered age is " + age + " but has to be above 18");
+            }
+            catch (FormatException fe)
+            {
+                Console.WriteLine("Invalid input for number");
+                Debug.WriteLine(fe.Message);
+            }
+            catch (InvalidAgeException iae)
+            {
+                Console.WriteLine("Age has to be above 18");
+                Debug.WriteLine(iae.Message);
+            }
+        }
+        static void Main(string[] args)
+        {
+            new Program().Age();
+
+        }
+    }
+}
